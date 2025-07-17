d71aff94c3039085ba7e62f7eba5462794c607b0
Manoj N <manoj8112014@gmail.com>
2025-07-17 09:21:57 +0530

    post route

--- ThoughtPulse-main/api/routes/post.route.js
+++ ThoughtPulse-main/api/routes/post.route.js
@@ -1,13 +1,50 @@
-import express from 'express'
-import { create, deletepost, getpost, getposts, updatepost } from '../controllers/post.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import {
+  create,
+  deletepost,
+  getpost,
+  getposts,
+  updatepost,
+  likePost,
+  unlikePost,
+  addComment,
+  deleteComment,
+  getPostsByCategory,
+  getPostsByUser,
+} from "../controllers/post.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post('/create', verifyToken, create);
-router.get('/getposts', getposts);
-router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
-router.put('/updatepost/:postId/:userId', verifyToken, updatepost);
-router.get('/:postId', getpost);
+// Create a new post
+router.post("/create", verifyToken, create);
 
-export default router;
+// Get all posts
+router.get("/getposts", getposts);
+
+// Get posts by category
+router.get("/category/:category", getPostsByCategory);
+
+// Get posts by user
+router.get("/user/:userId", getPostsByUser);
+
+// Get a single post by ID
+router.get("/:postId", getpost);
+
+// Update a post
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
+
+// Delete a post
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+
+// Like a post
+router.post("/like/:postId", verifyToken, likePost);
+
+// Unlike a post
+router.post("/unlike/:postId", verifyToken, unlikePost);
+
+// Add a comment to a post
+router.post("/comment/:postId", verifyToken, addComment);
+
+// Delete a comment from a post
+router.delete("/comment/:postId/:commentId", verifyToken, deleteComment);
